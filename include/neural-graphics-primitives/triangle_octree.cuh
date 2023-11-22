908e12715eb6fbc40d43a4a9e12314d58dbe2d9a
Xcy <20449924+xcyffl@users.noreply.github.com>
2023-11-22 14:56:09 -0400

    Update triangle_octree.cuh

--- include/neural-graphics-primitives/triangle_octree.cuh
+++ include/neural-graphics-primitives/triangle_octree.cuh
@@ -302,6 +302,7 @@ public:
 		float mint = MAX_DIST;
 
 		// Ensure that closer children are checked last such that they rise to the top of the stack
+		// Based on the sign of the ray direction, setting mask order.
 		uint8_t reorder_mask = 0;
 		if (rd.x > 0) reorder_mask |= 1;
 		if (rd.y > 0) reorder_mask |= 2;
@@ -311,26 +312,29 @@ public:
 			int idx = query_stack.pop();
 
 			const TriangleOctreeNode& node = nodes[idx];
-
+			
 			for (uint8_t aux = 0; aux < 8; ++aux) {
+				// e.g. If ray directions are all positive, then reorder_mask = 111, aux0: 111, aux1: 110, ..., deeper child are processed first (further from the ray)
 				uint8_t i = aux ^ reorder_mask;
 				int child_idx = node.children[i];
-
+				// Depth of the child.
 				uint8_t depth = node.depth+1;
 				// Intermediate leaf
 				if (child_idx == -1) {
 					continue;
 				}
-
+				// Base position.
 				u16vec3 pos = node.pos * (uint16_t)2;
 				if (i&1) ++pos.x;
 				if (i&2) ++pos.y;
 				if (i&4) ++pos.z;
 
 				float size = scalbnf(1.0f, -depth);
-
+				// Size of the bounding box reduces with the depth.
 				BoundingBox bb = {size * vec3(pos), size * vec3(pos + u16vec3(1))};
+				// testing the ray bounding box intersection.
 				vec2 t = bb.ray_intersect(ro, rd);
+				// return the intersection t.
 				if (t.y >= 0 && t.y < MAX_DIST && t.x < mint) {
 					// All children's children are gonna be leaves,
 					// so we can cut to the chase and terminate here already.
